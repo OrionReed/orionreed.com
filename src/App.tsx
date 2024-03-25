810e155810d815311d3646bfcc1e01bf315d4a38
Orion Reed <orionreedt@gmail.com>
2024-03-25 08:08:36 -0700

    add vercel 'line go up down' injector

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { inject } from '@vercel/analytics';
 import "@tldraw/tldraw/tldraw.css";
 import "@/css/style.css"
 import React, { useEffect, useState } from "react";
@@ -9,6 +10,7 @@ import { useCanvas } from "@/hooks/useCanvas"
 import { createShapes } from "@/utils";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Contact } from "@/components/Contact";
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
 
@@ -23,6 +25,7 @@ function App() {
 					<Route path="/card/contact" element={<Contact />} />
 				</Routes>
 			</BrowserRouter>
+			<Analytics />
 		</React.StrictMode>
 	);
 };
