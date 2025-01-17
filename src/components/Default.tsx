1d2c12b04e0ae50872f788551e502420420ef067
Orion Reed <orionreedt@gmail.com>
2025-01-17 19:54:25 +0100

    update

--- src/components/Default.tsx
+++ src/components/Default.tsx
@@ -1,71 +1,99 @@
 export function Default() {
   return (
     <main>
-      <header>
-        Orion Reed
-      </header>
+      <header>Orion Reed</header>
       <h2>Hello! 👋</h2>
       <p>
         My research investigates the intersection of computing, human-system
-        interfaces, and emancipatory politics. I am interested in the
-        potential of computing as a medium for thought, as a tool for
-        collective action, and as a means of emancipation.
+        interfaces, and emancipatory politics. I am interested in the potential
+        of computing as a medium for thought, as a tool for collective action,
+        and as a means of emancipation.
       </p>
 
       <p>
         My current focus is basic research into the nature of digital
         organisation, developing theoretical toolkits to improve shared
-        infrastructure, and applying this research to the design of new
-        systems and protocols which support the self-organisation of knowledge
-        and computational artifacts.
+        infrastructure, and applying this research to the design of new systems
+        and protocols which support the self-organisation of knowledge and
+        computational artifacts.
       </p>
 
       <h2>My work</h2>
       <p>
-        Alongside my independent work I am a researcher at <a href="https://block.science/">Block Science</a> building{' '}
-        <i>knowledge organisation infrastructure</i> and an engineer-in-residence at <a href="https://tldraw.com">tldraw</a>. I am also part of the nascent <a href="https://libcomp.org/">Liberatory Computing</a>{' '}
-        collective, occasional collaborator with <a href="https://economicspace.agency/">ECSA</a> and a co-organiser of the <a href="https://canvasprotocol.org/">OCWG</a>.
+        Alongside my independent work I am a researcher at{" "}
+        <a href="https://block.science/">Block Science</a> building{" "}
+        <i>knowledge organisation infrastructure</i> and an engineer/researcher
+        at <a href="https://websim.ai">websim</a>. I am also part of the nascent{" "}
+        <a href="https://libcomp.org/">Liberatory Computing</a> collective,
+        occasional collaborator with{" "}
+        <a href="https://economicspace.agency/">ECSA</a> and a co-organiser of
+        the <a href="https://canvasprotocol.org/">OCWG</a>.
       </p>
 
       <h2>Get in touch</h2>
       <p>
-        I am on Twitter <a href="https://twitter.com/OrionReedOne">@OrionReedOne</a>,
-        Mastodon <a href="https://hci.social/@orion">@orion@hci.social</a> and GitHub <a href="https://github.com/orionreed">@orionreed</a>. You can also shoot me an email <a href="mailto:me@orionreed.com">me@orionreed.com</a>
+        I am on Bsky{" "}
+        <a href="https://bsky.app/profile/orionreed.bsky.social">@OrionReed</a>,
+        Twitter <a href="https://twitter.com/OrionReedOne">@OrionReedOne</a>,
+        Mastodon <a href="https://hci.social/@orion">@orion@hci.social</a> and
+        GitHub <a href="https://github.com/orionreed">@orionreed</a>. You can
+        also shoot me an email{" "}
+        <a href="mailto:me@orionreed.com">me@orionreed.com</a>
       </p>
 
       <span className="dinkus">***</span>
 
       <h2>Talks</h2>
       <ol reversed>
-        <li><a
-          href="https://www.youtube.com/watch?v=csGNVaB83Rk">Spatial
-          Canvases: Towards an Integration Domain for HCI @ TfT Rocks 2024</a> (<a href="artifact/tft-rocks-integration-domain.pdf">slides</a>)
+        <li>
+          <a href="https://www.youtube.com/watch?v=csGNVaB83Rk">
+            Spatial Canvases: Towards an Integration Domain for HCI @ TfT Rocks
+            2024
+          </a>{" "}
+          (<a href="artifact/tft-rocks-integration-domain.pdf">slides</a>)
         </li>
-        <li><a
-          href="https://www.youtube.com/watch?v=-q-kk-NMFbA">Knowledge Organisation Infrastructure Demo @ NPC
-          Denver</a></li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=-q-kk-NMFbA">
+            Knowledge Organisation Infrastructure Demo @ NPC Denver
+          </a>
+        </li>
       </ol>
       <h2>Writing</h2>
       <ol reversed>
-        <li><a
-          href="/posts/scoped-propagators">Scoped Propagators: A programming model for spatial canvases</a></li>
-        <li><a
-          href="https://blog.block.science/objects-as-reference-toward-robust-first-principles-of-digital-organization/">Objects
-          as Reference: Toward Robust First Principles of Digital Organization</a></li>
+        <li>
+          <a href="/posts/scoped-propagators">
+            Scoped Propagators: A programming model for spatial canvases
+          </a>
+        </li>
+        <li>
+          <a href="https://blog.block.science/objects-as-reference-toward-robust-first-principles-of-digital-organization/">
+            Objects as Reference: Toward Robust First Principles of Digital
+            Organization
+          </a>
+        </li>
       </ol>
       <h2>Things I made recently</h2>
       <ol reversed>
-        <li><a
-          href="https://twitter.com/OrionReedOne/status/1772934478421188620">Tiny 3D HTML/DOM viewer (you can paste it into your console)</a>
+        <li>
+          <a href="https://twitter.com/OrionReedOne/status/1772934478421188620">
+            Tiny 3D HTML/DOM viewer (you can paste it into your console)
+          </a>
         </li>
-        <li><a
-          href="https://twitter.com/OrionReedOne/status/1772271156373905465">DOM to canvas translation (it's that button in the top right of this page)</a>
+        <li>
+          <a href="https://twitter.com/OrionReedOne/status/1772271156373905465">
+            DOM to canvas translation (it's that button in the top right of this
+            page)
+          </a>
         </li>
-        <li><a
-          href="https://github.com/OrionReed/tldraw-graph-layout">Constraint-based graph layout in tldraw</a>
+        <li>
+          <a href="https://github.com/OrionReed/tldraw-graph-layout">
+            Constraint-based graph layout in tldraw
+          </a>
         </li>
-        <li><a
-          href="https://github.com/OrionReed/tldraw-physics">Physics in tldraw</a>
+        <li>
+          <a href="https://github.com/OrionReed/tldraw-physics">
+            Physics in tldraw
+          </a>
         </li>
       </ol>
     </main>
